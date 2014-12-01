e2b6a7d5a7f4dc4caeb237a91f0827ddcfe3a1d0
lightszero <lightsever@hotmail.com>
2014-12-01 15:45:26 +0800

    571

--- core/CSLightEvil/C#LE/CLS_Environment.cs
+++ core/CSLightEvil/C#LE/CLS_Environment.cs
@@ -18,7 +18,7 @@ namespace CSLE
         {
             get
             {
-                return "0.57Beta";
+                return "0.57.1Beta";
             }
         }
         public CLS_Environment(ICLS_Logger logger)
